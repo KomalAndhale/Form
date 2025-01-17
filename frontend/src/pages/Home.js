6da3312721479757f7b978d6c19c5caf8195e8b5
Komal Andhale <160253660+KomalAndhale@users.noreply.github.com>
2025-01-17 17:00:12 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:3006/products";
+            const url = "https://form-koey.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
